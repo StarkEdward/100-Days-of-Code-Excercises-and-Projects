65f7223ffb72cb4661ecbbb45e10d89584b945e4
StarkEdward <skamble454@gmail.com>
2022-05-06 00:11:58 +0530

    day 6 project working

---
+++ Day 06/Day 6 Final Project Maze  challange.py
@@ -0,0 +1,20 @@
+# Visit this link and copy paste or type the below code to run.
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+
+# Note: This code has some bugs... the robot gets to infinite loop on some position.
