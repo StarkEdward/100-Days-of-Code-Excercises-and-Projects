42ccd73e7626d7ba12b7663d193117a7563072f2
StarkEdward <skamble454@gmail.com>
2022-05-07 20:56:08 +0530

    day 7 started

---
+++ Day 07/Day 7 Check the letter present in word or not.py
@@ -0,0 +1,5 @@
+import random
+# step 1
+word_list = ["elephant", 'horse', 'camel', 'baboon']
+
+#  TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
