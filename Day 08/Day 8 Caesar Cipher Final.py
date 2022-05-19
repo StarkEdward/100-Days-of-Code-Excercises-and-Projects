a1141dcd27c38bab01ea3247ee45b3f1c4036a31
StarkEdward <skamble454@gmail.com>
2022-05-19 13:03:17 +0530

    Day 8 Final

--- Day 08/Day 8 Caesar Cipher Final.py
+++ Day 08/Day 8 Caesar Cipher Final.py
@@ -1,6 +1,5 @@
 # Caesar Cipher Final code
 
-# Caesar Cipher Final code
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
@@ -21,14 +20,18 @@ def caesar(start_text, shift_amount, cipher_direction):
 
 while True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    try:
+        if direction == 'encode' or direction == 'decode':
+            text = input("Type your message:\n").lower()
+            shift = int(input("Type the shift number:\n"))
 
-    shift = shift % 26
+            shift = shift % 26
 
-    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+        caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-    result = input("Type 'yes' if you want to try agian. Otherwise type 'no'\n")
-    if result == 'no':
-        print("Good bye!")
-        break
+        result = input("Type 'yes' if you want to try again. Otherwise type 'no'\n")
+        if result == 'no':
+            print("Good bye!")
+            break
+    except :
+        print("Invalid Input, please re-enter.")
