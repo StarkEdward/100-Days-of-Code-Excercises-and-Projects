4e9513e72a7d9bbc78877288945aa4e115b22995
StarkEdward <skamble454@gmail.com>
2022-05-24 21:07:33 +0530

    Day 9

--- Day 09/Day 9.1.py
+++ Day 09/Day 9.1.py
@@ -1,2 +1,12 @@
 # Grading Program
 
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74
+    "Neville": 62
+}
+
+# Creating an empty dictionary
+student_grades = {}
