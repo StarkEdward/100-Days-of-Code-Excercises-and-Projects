05620a73745a4c8252bb0d8357ac3d0809945fe6
StarkEdward <skamble454@gmail.com>
2022-05-26 19:24:05 +0530

    Day 9 final project done

--- Day 09/Day 9 Final - Blind Auction/main.py
+++ Day 09/Day 9 Final - Blind Auction/main.py
@@ -5,9 +5,9 @@ import art
 print(art.logo)
 print("Welcome to the Secret Auction Program.")
 
-bidder_record = {}
+bidder_record = {} # Creating an empty Dict to keep track of entries,
 
-
+# Creating function to find out highest bidder and winner name.
 def find_highest_bidder(bid):
     highest_bid = 0
     winner = ""
@@ -16,7 +16,7 @@ def find_highest_bidder(bid):
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
-    print(f"The winner is {winner} with the bid of ₹{highest_bid}.")
+    print(f"The winner is {winner} with the bid of ₹{highest_bid}.")    #Disply the final output
 
 
 while True:
@@ -26,7 +26,8 @@ while True:
     choice = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
 
     if choice == 'no':
-        find_highest_bidder(bid=bidder_record)
+        find_highest_bidder(bid=bidder_record)      # calling the function
         break
     else:
-        os.system("cls")
+        os.system("cls")        # clear the screen for Windows
+    #     os.system("clear")      # clear the screen for Linux
