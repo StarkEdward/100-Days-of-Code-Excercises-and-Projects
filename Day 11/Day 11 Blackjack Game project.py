358353c72fed78f359c6596f4d5d41b13c37ef5b
StarkEdward <skamble454@gmail.com>
2022-05-28 21:15:00 +0530

    Day 11 Blackjack Project Start

--- Day 11/Day 11 Blackjack Game project.py
+++ Day 11/Day 11 Blackjack Game project.py
@@ -18,3 +18,12 @@ for _ in range(2):
 # Testing output of user_card & computer_card
 print(user_cards)
 print(computer_cards)
+
+
+# creating a function to calculate score
+def calculate_score():
+    user_score = sum(user_cards)
+    return user_score
+
+
+print(f"Your cards {user_cards}, current Score: {calculate_score()}")
