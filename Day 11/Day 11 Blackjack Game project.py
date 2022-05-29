74a0c2f7297ca132035ba6207145d46edac0d7e7
StarkEdward <skamble454@gmail.com>
2022-05-29 11:43:25 +0530

    Day 11 Blackjack Project Start

--- Day 11/Day 11 Blackjack Game project.py
+++ Day 11/Day 11 Blackjack Game project.py
@@ -3,13 +3,14 @@ import random
 
 
 def deal_card():
-    '''Returns a random card from a deck.'''
+    """Returns a random card from a deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
+
 def calculate_score(cards):
-    '''Take a list from cards and return the score calculated form cards'''
+    """Take a list from cards and return the score calculated form cards"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
@@ -17,6 +18,7 @@ def calculate_score(cards):
         cards.remove(11)
         cards.append(1)
     return sum(cards)
+
 
 user_cards = []
 computer_cards = []
@@ -26,12 +28,24 @@ for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-# Testing output of user_card & computer_card
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first card: {computer_cards[0]}")
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    # Testing output of user_card & computer_card
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Press 'y' to get another card, type 'n' to pass: ")
+
+        if user_should_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 or computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
