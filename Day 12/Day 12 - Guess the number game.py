c5f83f984274e2852c8962fe5ddaea27c3f3fc14
StarkEdward <skamble454@gmail.com>
2022-05-30 21:30:31 +0530

    Day 12 Guess the name game start

--- Day 12/Day 12 - Guess the number game.py
+++ Day 12/Day 12 - Guess the number game.py
@@ -1,7 +1,8 @@
 from random import randint
 
-EASY_LEVEL = 10
-HARD_LEVEL = 5
+# global constant
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 
 # function to check  user's guess against actual answer
@@ -11,16 +12,16 @@ def check_answer(guess, answer):
     elif guess < answer:
         print("Too low")
     else:
-        print("You got it! The answer was {answer}")
+        print(f"You got it! The answer was {answer}")
 
 
 # make function to set difficulty
 def check_difficulty():
     level = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if level == 'easy':
-        print(f"You have {EASY_LEVEL} attempts remaining to guess the number.")
+        return EASY_LEVEL_TURNS
     else:
-        print(f"You have {HARD_LEVEL} attempts remaining to guess the number")
+        return HARD_LEVEL_TURNS
 
 
 # Choosing a random number between 1 and 100
@@ -31,7 +32,8 @@ print(f"The Correct answer is: {answer}")
 
 # let the user guess number
 guess = int(input("Make a guess: "))
-
+turns = check_difficulty()
+print(f"You have {turns} attempts remaining to guess the number.")
 # Track the number of urns and reduce by 1 if they get it wrong.
 
 # Repeat the guessng functionality if they get it wrong.
