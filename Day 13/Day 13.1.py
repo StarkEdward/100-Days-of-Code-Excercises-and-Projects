4d2a050de785ca1f1dff31c1254bc9aa874fd4f4
StarkEdward <skamble454@gmail.com>
2022-06-01 22:01:34 +0530

    Day 12 Guess the name game done

---
+++ Day 13/Day 13.1.py
@@ -0,0 +1,14 @@
+# fix the bug challenge
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+# # debugging solution
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
