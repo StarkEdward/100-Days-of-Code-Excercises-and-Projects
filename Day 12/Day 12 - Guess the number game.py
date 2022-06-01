24ea912901905afc5d48e95f4094ae7b09605fc6
StarkEdward <skamble454@gmail.com>
2022-06-01 18:13:22 +0530

    Day 12 Guess the name game done

--- Day 12/Day 12 - Guess the number game.py
+++ Day 12/Day 12 - Guess the number game.py
@@ -6,11 +6,13 @@ HARD_LEVEL_TURNS = 5
 
 
 # function to check  user's guess against actual answer
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
     if guess > answer:
         print("Too High.")
+        return turns - 1
     elif guess < answer:
         print("Too low")
+        return turns - 1
     else:
         print(f"You got it! The answer was {answer}")
 
@@ -23,17 +25,27 @@ def set_difficulty():
     else:
         return HARD_LEVEL_TURNS
 
+def game():
+    # Choosing a random number between 1 and 100
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"The Correct answer is: {answer}")
+    turns = set_difficulty()
+    # Repeat the guessing functionality if they get it wrong.
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        # let the user guess number
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
 
-# Choosing a random number between 1 and 100
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
-print(f"The Correct answer is: {answer}")
+        if turns == 0:
+            print("You have run out of guesses. You Lose!")
+            print(f"The correct answer is: {answer}")
+            return
+        elif guess != answer:
+            print("Guess again")
 
-# let the user guess number
-guess = int(input("Make a guess: "))
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number.")
-# Track the number of urns and reduce by 1 if they get it wrong.
+game()
 
-# Repeat the guessng functionality if they get it wrong.
