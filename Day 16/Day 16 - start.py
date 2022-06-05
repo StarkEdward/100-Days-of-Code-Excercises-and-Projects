f5591f16bf2077650f63e9de437387bb489d5b0e
StarkEdward <skamble454@gmail.com>
2022-06-05 21:19:44 +0530

    day 16 start

---
+++ Day 16/Day 16 - start.py
@@ -0,0 +1,11 @@
+# from turtle import Turtle, Screen
+# jimmy = Turtle()
+# print(jimmy)
+# jimmy.shape("turtle")
+# jimmy.color("red")
+# jimmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
