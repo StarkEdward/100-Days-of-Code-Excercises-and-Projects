476a989059dc90ab29bc41e163b38f7bb4d93720
StarkEdward <skamble454@gmail.com>
2022-06-06 22:12:09 +0530

    day 17 Quiz Game start

---
+++ Day 17/quiz game/quiz_brain.py
@@ -0,0 +1,16 @@
+# TODO: Asking the question
+# TODO: Checking if the answer was correct
+# TODO: chcing if we're the end of the quiz
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+
+
