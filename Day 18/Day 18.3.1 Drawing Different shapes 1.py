7bff3522a667cecd605cdbc048b8136e3c5d935a
StarkEdward <skamble454@gmail.com>
2022-06-08 19:55:15 +0530

    day 18.3.1

--- Day 18/Day 18.3.1 Drawing Different shapes 1.py
+++ Day 18/Day 18.3.1 Drawing Different shapes 1.py
@@ -1,17 +1,21 @@
 from turtle import Turtle, Screen
 import random
 shapes = Turtle()
+
+
 def triangle():
     shapes.color("blue")
     for _ in range(3):
         shapes.forward(100)
         shapes.right(120)
 
+
 def square():
     shapes.color("orange")
     for _ in range(4):
         shapes.forward(100)
         shapes.right(90)
+
 
 def pentagon():
     shapes.color("lime green")
@@ -26,11 +30,13 @@ def hexagon():
         shapes.forward(100)
         shapes.right(60)
 
+
 def septagon():
     shapes.color("deep sky blue")
     for _ in range(7):
         shapes.forward(100)
         shapes.right(51.43)
+
 
 def octogon():
     shapes.color("light coral")
@@ -38,19 +44,19 @@ def octogon():
         shapes.forward(100)
         shapes.right(45)
 
+
 def nonagon():
     shapes.color("orange")
     for _ in range(9):
         shapes.forward(100)
         shapes.right(40)
 
+
 def decagon():
     shapes.color("blue violet")
     for _ in range(10):
         shapes.forward(100)
         shapes.right(36)
-
-
 
 
 triangle()
