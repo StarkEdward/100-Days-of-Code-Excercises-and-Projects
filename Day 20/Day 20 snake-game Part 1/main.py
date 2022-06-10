28b942df6aafadadeac3c32a5a82b657b2ea7cf1
StarkEdward <skamble454@gmail.com>
2022-06-10 21:32:52 +0530

    day 20

--- Day 20/Day 20 snake-game Part 1/main.py
+++ Day 20/Day 20 snake-game Part 1/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,40 +8,14 @@ screen.bgcolor("black")
 screen.title("Snake Game by Sagar Kamble.")
 screen.tracer(0)
 
-starting_position = [(0,0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_position:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 
 
