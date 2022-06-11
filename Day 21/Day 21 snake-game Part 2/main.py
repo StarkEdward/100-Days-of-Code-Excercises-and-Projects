a8ff223ebad1ce0e4d501949086de2f726c29de9
StarkEdward <skamble454@gmail.com>
2022-06-11 22:13:02 +0530

    day 20

--- Day 21/Day 21 snake-game Part 2/main.py
+++ Day 21/Day 21 snake-game Part 2/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +13,8 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 screen.listen()
+scoreboard = Scoreboard()
+
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -19,8 +23,12 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collison with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 
 
