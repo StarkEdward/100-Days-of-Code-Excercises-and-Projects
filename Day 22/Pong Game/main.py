0ca0a5136b507bd034758af5c69258c8d24ddbb7
StarkEdward <skamble454@gmail.com>
2022-06-12 16:57:08 +0530

    day 22

--- Day 22/Pong Game/main.py
+++ Day 22/Pong Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,15 +10,11 @@ screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-# paddle = Turtle("square")
-# paddle.color("white")
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-# paddle.penup()
-# paddle.goto(x=350, y=0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -40,6 +37,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
 
+    # detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
