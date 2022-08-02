2d47505835a83cb893369a51e2bd06c120ba6ca6
StarkEdward <skamble454@gmail.com>
2022-08-02 23:54:57 +0530

    day 25

---
+++ Day 25/us-states-game-start/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_state = data.state.to_list()
+guessed_state = []
+# get the co-ordinate
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 Guess the State",
+                                    prompt="What's another state's name?").title()
+    # if answer_state is one of the state in all the states of the 50_states.csv.
+    if answer_state == "Exit":
+        break
+    if answer_state in all_state:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))  # if they got it right
+        # create a turtle to write the name of the state at the state's x and y co-ordinates.
+        # t.write(state_data.state.item())
+        t.write(answer_state)
+
+# states to learn.csv
+
