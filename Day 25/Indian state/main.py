7b6873a2b3c5c6506c6beb6d88f61ee1540d86a1
StarkEdward <skamble454@gmail.com>
2022-08-03 21:14:10 +0530

    working on indian state quiz

--- Day 25/Indian state/main.py
+++ Day 25/Indian state/main.py
@@ -7,14 +7,4 @@ image = "Indian_states.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-ordinate = []
 
-def get_mouse_coordinates(x, y):
-    x = ordinate.append(x)
-    y = ordinate.append(y)
-    # print(x, y)
-    print(ordinate)
-
-
-turtle.onscreenclick(get_mouse_coordinates)
-turtle.mainloop()
